8346b6628f2abc0133b0fef8bd503e47a21ffd7c
hzhyvinskyi <hzhyvinskyi@gmail.com>
2019-03-09 21:18:28 +0200

    Added twig templates for Blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,9 +4,18 @@ namespace App\Controller;
 
 use App\Service\Greeting;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
+/**
+ * Class BlogController
+ * @package App\Controller
+ * @Route("/blog")
+ */
 class BlogController extends AbstractController
 {
     /**
@@ -14,20 +23,68 @@ class BlogController extends AbstractController
      */
     private $greeting;
 
-    public function __construct(Greeting $greeting)
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    public function __construct(Greeting $greeting, RouterInterface $router, SessionInterface $session)
     {
         $this->greeting = $greeting;
+        $this->router = $router;
+        $this->session = $session;
     }
 
     /**
-     * @param Request $request
+     * @Route("/", name="blog_index")
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/blog", name="blog_index")
      */
-    public function index(Request $request)
+    public function index(): Response
     {
-        return $this->render('base.html.twig', [
-            'message' => $this->greeting->greet($request->get('name'))
+        return $this->render('blog/index.html.twig', [
+            'posts' => $this->session->get('posts')
+        ]);
+    }
+
+    /**
+     * @Route("/store", name="blog_store")
+     * @return RedirectResponse
+     * @throws \Exception
+     */
+    public function store(): RedirectResponse
+    {
+        $posts = $this->session->get('posts');
+        $posts[uniqid()] = [
+            'title' => 'Post example title ' . rand(1, 1000),
+            'body' => 'Post example body ' . rand(1, 1000),
+            'date' => new \DateTime()
+        ];
+        $this->session->set('posts', $posts);
+
+        return new RedirectResponse($this->router->generate('blog_index'));
+    }
+
+    /**
+     * @Route("/{id}", name="blog_show")
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show($id): Response
+    {
+        $posts = $this->session->get('posts');
+
+        if (!$posts || !isset($posts[$id])) {
+            throw new NotFoundHttpException('Post not found');
+        }
+
+        return $this->render('blog/show.html.twig', [
+            'id' => $id,
+            'post' => $posts[$id]
         ]);
     }
 }
